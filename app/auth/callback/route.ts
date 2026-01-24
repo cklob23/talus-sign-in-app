441e6b5a0583f1ebdc4f72c2bb5b43a433868298
Caleb Klobe <klobe.caleb23@gmail.com>
2026-01-24 14:20:25 -0500

    update callback

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,14 +1,18 @@
-import { createClient } from "@/lib/supabase/client"
+// app/auth/callback/route.ts
+import { createClient } from "@/lib/supabase/server"
 import { NextResponse } from "next/server"
 
 export async function GET(request: Request) {
-  const { searchParams, origin } = new URL(request.url)
+  const { searchParams } = new URL(request.url)
   const code = searchParams.get("code")
-    console.log(origin)
+
   if (code) {
     const supabase = createClient()
-    await supabase.auth.exchangeCodeForSession(code)
+    await (await supabase).auth.exchangeCodeForSession(code)
   }
 
-  return NextResponse.redirect(`${origin}/admin`)
+  const redirectBase =
+    process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000"
+
+  return NextResponse.redirect(`${redirectBase}/admin`)
 }
